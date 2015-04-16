060bac0584c402781558a05d389eb04a698ebebe
Александр <sanuek@yandex.by>
2015-04-16 22:52:59 +0300

    Name
    La La

---
+++ blogg/add.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sample</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <?
+    $f=fopen("text.txt","a") or die("Ошибка! Невозможно открыть файл text.txt");
+    flock ($f,LOCK_EX);
+    {
+	   $t1=$_GET['subject'];
+	   $t2=$_GET['name'];
+	   $t3=$_GET['area'];
+	   fputs($f,"\r".$t1."\r\n");
+	   fputs($f,$t2."\r\n");
+	   fputs($f,$t3."\r\n");
+    }
+    flock ($f,LOCK_UN);
+    fclose($f);
+    ?>
+   <div class="background"></div>
+    <div class="content">
+       <h1>Yet another blog</h1>
+       <section class="left">
+           <nav>
+               <ul>
+                   <li><a href="index.php">Отзывы</a></li>
+                   <li><a href="add.php">Добавить</a></li>
+                   <li><a href="">Раздел три</a></li>
+                   <li><a href="">Раздел четыре</a></li>
+               </ul>
+           </nav>
+       </section>
+       <section class="right">
+            <h2>Написать отзыв</h2>
+            <form action="<?=$SCRIPT_NAME?>" method="get">
+                <input type="text" name="subject" placeholder="Тема сообщения">
+                <input type="text" name="name" placeholder="Имя">
+                <textarea name="area" id=""></textarea>
+                <input type="submit" value="отправить">
+            </form>
+       </section>
+    </div>
+</body>
+</html>
